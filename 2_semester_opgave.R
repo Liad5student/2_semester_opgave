3f9796689c210b84b0cb46de35c1cdcd2300ccb3
Martin55530 <55530@student.eadania.dk>
2025-04-03 21:07:01 +0200

    Update 2_semester_opgave.R

--- 2_semester_opgave.R
+++ 2_semester_opgave.R
@@ -1,1 +1,13 @@
-# Nye variabler
+# Nye variabler (foreløbige ideer)
+
+# Virksomhedens alder
+
+# Antal events deltaget i
+
+# Kontingentniveau
+
+# Har deltaget i minimum 1 event
+
+# Branche hovedgruppe
+
+# Område ud fra postnummer
