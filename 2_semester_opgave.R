23c086b18cd75f4bf1d3c3fec68694d714fb27c2
Martin55530 <55530@student.eadania.dk>
2025-04-03 22:38:33 +0200

    kommentar

--- 2_semester_opgave.R
+++ 2_semester_opgave.R
@@ -4,6 +4,6 @@ library(dplyr)
 # Indlæs det gemte objekt fra .rds-filen
 merged_unique <- readRDS("merged_unique.rds")
 
-# Ændrer variabel navne
+# Ændrer true/false til 1/0 så vi kan bruge det i churn analysen
 merged_unique <- merged_unique |> 
   mutate(across(where(is.logical), ~ as.integer(.)))
