53fc18ad6f1a9d8fb17f582f641eb7efc96a0294
Martin55530 <55530@student.eadania.dk>
2025-04-03 22:35:23 +0200

    Begyndt på dataklargøring

--- 2_semester_opgave.R
+++ 2_semester_opgave.R
@@ -1,2 +1,9 @@
-# test
-#abdi
+# Indlæser og loader pakker
+library(dplyr)
+
+# Indlæs det gemte objekt fra .rds-filen
+merged_unique <- readRDS("merged_unique.rds")
+
+# Ændrer variabel navne
+merged_unique <- merged_unique |> 
+  mutate(across(where(is.logical), ~ as.integer(.)))
