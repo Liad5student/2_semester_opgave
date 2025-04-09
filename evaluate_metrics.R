58759e41c8fb74b14eee7627aeb7b96dcc4b5a27
Martin55530 <55530@student.eadania.dk>
2025-04-09 11:15:45 +0200

    Update evaluate_metrics.R

--- evaluate_metrics.R
+++ evaluate_metrics.R
@@ -1,3 +1,20 @@
+# Workflow
+
+# 1. Load data             # Læs filer fra fx CSV, Excel, database, API
+# 2. Merge datasets        # Join flere datakilder (demografi, køb, logs...)
+# 3. Clean data            # Ryd op: navne, NA, duplicates, outliers
+# 4. Feature Engineering   # Skab nye variable, fx "dage siden sidste køb"
+# 5. EDA                   # Forstå churn, fordelinger, sammenhænge
+# 6. Preprocessing         # Split, recipe, dummy, scale, håndtér ubalance
+# 7. Modelling             # Fit/tune modeller (logistic, RF, XGBoost...)
+# 8. Evaluate metrics      # ROC, confusion matrix, AUC, fortolkning
+
+# 9. Main (Quarto)
+
+# ------------------------------------------------------------------------------
+# Pacman
+# ------------------------------------------------------------------------------
+
 # Indlæser nødvendige pakker med pacman (installerer automatisk hvis mangler)
 pacman::p_load(
     dplyr,     # til datamanipulation
