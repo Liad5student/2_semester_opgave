5786a84ba1176e0dc07ceea334f63e1186538434
Martin55530 <55530@student.eadania.dk>
2025-04-09 19:56:14 +0200

    Fra Bjarnes fil - skal rettes til

--- preprocessing.R
+++ preprocessing.R
@@ -60,74 +60,30 @@ pacman::p_load(
 )
 
 # Helper-funktion: Henter og opdaterer .rds-filer fra de andre branches
-
 source("load_all_data.R")
+
+# Henter .rds-fil fra branchet EDA
+eda <- readRDS("data/eda.R")
 
 # ------------------------------------------------------------------------------
 # 6. Preprocessing
 # ------------------------------------------------------------------------------
 
-eda <- readRDS("data/eda.R")
+# Fra Bjarnes fil - skal rettes til
 
-# Split data i træning og test
-set.seed(2025)
-vff_split <- initial_split(vffXXXX, prop = 0.8, strata = tilskuere)
-vff_train <- training(vff_split)
-vff_test  <- testing(vff_split)
+set.seed(2024)
+churn_split <- initial_split(churn_data, prop = 0.8, strata = churn)
+churn_train <- training(churn_split)
+churn_test <- testing(churn_split)
 
-# Resampling-metode er bootstrapping
-vff_boost <- bootstraps(vff_train, times = 25, strata = tilskuere)  
-# Bootstrapping er en statistisk metode, der involverer gentagen 
-# sampling fra et (trænings)datasæt med tilbagelægning. Herved får
-# man altid det samme antal observationer til træning af tuningparametrene, 
-# men de forskellige stikprøver kan indeholde forskellige observationer og
-# forskelligt antal observationer, hvilket giver mulighed for at evaluere 
-# modellen på forskellige datasæt og dermed forbedre valideringen.
+churn_folds <- vfold_cv(churn_train, v = 10, strata = churn)
 
-# ------------------------------------------------------------------
-# Recipes
-# ------------------------------------------------------------------
-
-recipe_scaled <- recipe(tilskuere ~ ., data = vff_train) |> 
-  # Definerer opskriften, hvor tilskuere er outcome, og alle 
-  # andre variabler (.) er prædiktorer.
-  step_nzv(all_numeric(), -all_outcomes()) |> 
-  # Fjerner numeriske variabler med næsten ingen varians (near-zero 
-  # variance). Disse variabler bidrager sjældent til modellen, da de 
-  # næsten ikke varierer.
-  step_corr(all_numeric_predictors(), threshold = 0.85) |> 
-  # Fjerner stærkt korrelerede numeriske prædiktorer 
-  # (korrelationskoefficient > 0.85). Dette reducerer redundans i data
-  # og hjælper med at undgå multikollinearitet.
-  step_novel(all_nominal_predictors()) |> 
-  # Håndterer nye niveauer i kategoriske variabler, der ikke var til 
-  # stede i træningsdata. Dette sikrer, at modellen kan håndtere ukendte 
-  # kategorier i fremtidige data.
-  step_normalize(all_numeric(), -all_outcomes()) |>  
-  # Normaliserer numeriske variabler ved at skalere dem til en 
-  # standardnormal fordeling (gennemsnit = 0, standardafvigelse = 1).
-  # Dette er vigtigt for modeller, der er følsomme over for skala, som 
-  # fx lineær regression 
-  step_YeoJohnson(all_numeric(), -all_outcomes()) |>  
-  # Anvender en Yeo-Johnson-transformation på numeriske variabler for at 
-  # gøre dem mere normale i deres fordeling. Dette kan forbedre modellens 
-  # præstation, især hvis data er skæve.
-  step_other(all_nominal(), threshold = 0.05) |>   
-  # Kombinerer sjældne kategorier i kategoriske variabler (frekvens < 5%) 
-  # til en fælles kategori ("other"). Dette reducerer støj fra sjældne 
-  # niveauer.
-  step_dummy(all_nominal_predictors(), one_hot = TRUE) 
-# Konverterer kategoriske variabler til one-hot encoded dummy-variabler. 
-# Hver kategori bliver repræsenteret som en binær variabel (0 eller 1).
-
-# Der er lavet to alternative recipes, som kan laves endnu mere 
-# avancerede ved fx at tilføje tuning til fx ovenstående thresholds.
-recipe_reduceret <- recipe(tilskuere ~ ., data = vff_train) |> 
-  step_nzv(all_numeric(), -all_outcomes()) |> 
+churn_recipe <- recipe(churn ~ ., data = churn_train) |>
   step_novel(all_nominal_predictors()) |>
-  step_dummy(all_nominal_predictors(), one_hot = TRUE)  
-
-
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
+  step_zv(all_predictors()) |>
+  step_normalize(all_numeric_predictors()) |>
+  step_smote(churn)  # hvis ubalanceret
 
 # ------------------------------------------------------------------------------
 # End
