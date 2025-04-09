33dbeb4a310368db8ea7735ff40616c701da7f6e
Martin55530 <55530@student.eadania.dk>
2025-04-09 10:03:46 +0200

    Update 2.-Merge-datasets.R

--- 2.-Merge-datasets.R
+++ 2.-Merge-datasets.R
@@ -1,2 +1,25 @@
-# test
-#abdi
+# ------------------------------------------------------------------------------
+# 2. Merge datasets
+# ------------------------------------------------------------------------------
+loadRDS("data/load_data.rds")
+
+jsdbfkjs
+lskafgnasdkjf
+njasdngf
+jsdbfkjs
+lskafgnasdkjf
+njasdngf
+jsdbfkjs
+lskafgnasdkjf
+njasdngf
+jsdbfkjs
+lskafgnasdkjf
+njasdngf
+jsdbfkjs
+lskafgnasdkjf
+njasdngf
+
+
+
+
+saveRDS("data/merge_datasets.rds")
