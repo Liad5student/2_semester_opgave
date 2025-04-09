541567bb8f941d41948992c937e5c7ed724e025d
Maria Cristiana Maxim <paduremariacristiana@yahoo.com>
2025-04-09 21:45:37 +0200

    Clean datasæt

--- 3.-Clean-data.R
+++ 3.-Clean-data.R
@@ -69,9 +69,74 @@ merge_datasets <- readRDS("data/merge_datasets.rds")
 # 3. Clean data
 # ------------------------------------------------------------------------------
 
-merge_datasets
+glimpse(merge_datasets)
 
+# Tjekker for NA værdier 
 
+na_count <- merge_datasets %>%
+  summarise(across(everything(), ~ sum(is.na(.)))) %>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "na_count")
+
+# Det er ikke noget problem med NA værdier i dataene, 
+# så vi kan gå videre til næste trin
+
+# Tjekker for duplicates
+
+duplicates_count <- merge_datasets %>%
+  group_by(CompanyId) %>%
+  summarise(duplicates = n()) %>%
+  filter(duplicates > 1)
+
+# Fjerne duplicates
+
+clean_data <- merge_datasets %>%
+  distinct(CompanyId, .keep_all = TRUE)
+
+# Antag at dit datasæt hedder df
+names(clean_data) <- names(clean_data) %>%
+  # Fjern indledende ID'er (f.eks. "26481_1")
+  str_remove("^[0-9]+_1*\\s*") %>%
+  # Erstat mellemrum, /, - og lign. med _
+  str_replace_all("[ /\\-]+", "_") %>%
+  # Fjern dobbelte underscores
+  str_replace_all("_+", "_") %>%
+  # Fjern evt. afsluttende underscore
+  str_remove("_$") %>%
+  # Trim whitespace og lav til lowercase
+  str_trim()
+  
+
+# Tjek de nye navne
+names(clean_data)
+
+# Fjern ID’er og target (som ikke skal bruges som features)
+
+clean_data <- clean_data %>%
+  select(-CompanyId, -ContactId, -CompanyOwnerId, -CVR, -PNumber,-EventId,
+         -EventExternalId,-EventPublicId,-LocationId, -Tekstfelt, -CompanyType)
+
+# Fjern "Tom", "Ukendt", "Ingen event" og lav til NA 
+
+clean_data <- clean_data %>%
+  mutate(across(where(is.character), ~na_if(.x, "Ukendt")))
+
+# Fjerner NA værdier 
+
+clean_data <- clean_data %>%
+  drop_na()
+
+# Konverter 'Employees' til numerisk
+
+ clean_data$Employees = as.numeric(clean_data$Employees)
+
+glimpse(clean_data)
+ 
+# Vi skal konverterer flere kolonner til den rigtige format fordi der 
+# er flere der er chr i stedet for numerisk mens vi har stadig "ingen event" i variabler
+# der skal være numerisk såsom meetinglength, max participants, EventLength 
+
+# Hvad skal vi laver med denne variabler? Jeg har prøvet at fjerne alle der har Tom
+# ingen event og ukendt og det kun er 60 observationer tilbage :(
 
 # ------------------------------------------------------------------------------
 # End
