353354f45e9895ee6a7fc215dfd24391271ea7e8
Martin55530 <55530@student.eadania.dk>
2025-04-09 10:28:12 +0200

    test

--- 1.-Load-data.R
+++ 1.-Load-data.R
@@ -1,3 +1,19 @@
-# test
-#abdi
-# HJE ALLE sammen
+# Indlæser nødvendige pakker med pacman (installerer automatisk hvis mangler)
+pacman::p_load(
+    dplyr,     # til datamanipulation
+    tidyr,     # til fx split af kolonner
+    stringr    # til teksthåndtering
+)
+
+# ------------------------------------------------------------------------------
+# 1. Load data
+# ------------------------------------------------------------------------------
+#loadRDS("data/load_data.rds")
+
+
+
+
+
+
+
+saveRDS(load_data, "data/load_data.rds")
